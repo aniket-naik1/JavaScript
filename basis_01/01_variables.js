db19496d6f269b888a83d2f6990e323dd4ab2c6c
Aniket Naik <aniketnaik1985@gmail.com>
2025-06-26 16:17:29 +0530

    good

--- basis_01/01_variables.js
+++ basis_01/01_variables.js
@@ -1,6 +1,13 @@
 const accountId = 144553
 let accountEmail = "aniket@gmail.com"
 var accountPassword = "12345"
+accountCity ="pune"
  
+accountEmail ="aj1883@gmail.com"
+accountPassword ="534343"
+accountCity = "mumbai"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
 console.log(accountEmail)
 console.log(accountId)
