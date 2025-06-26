e87cbfac94767be7ade6853a20c79a8eadbc129b
Aniket Naik <aniketnaik1985@gmail.com>
2025-06-26 17:18:44 +0530

    03

---
+++ basis_01/03_Conversionoroperation.js
@@ -0,0 +1,13 @@
+let score = "Aniket"
+console.log( typeof score);
+console.log(typeof(score));
+let valueInnumber  =Number(score)
+console.log(typeof valueInnumber);
+console.log(valueInnumber);
+
+//"33"=33
+// "33abc" =NaN
+//true=1
+let islog = 1
+let booleanIslog = Boolean(islog)
+console.log(booleanIslog);
