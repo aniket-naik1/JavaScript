0945de3b53aaa3bd5172eba2f2221a3e6202e1c6
Aniket Naik <aniketnaik1985@gmail.com>
2025-06-26 16:58:53 +0530

    legal

--- basis_01/01_variables.js
+++ basis_01/01_variables.js
@@ -2,6 +2,8 @@ const accountId = 144553
 let accountEmail = "aniket@gmail.com"
 var accountPassword = "12345"
 accountCity ="pune"
+console.table([accountId,accountEmail,accountPassword,accountCity]);
+
  
 accountEmail ="aj1883@gmail.com"
 accountPassword ="534343"
@@ -9,5 +11,5 @@ accountCity = "mumbai"
 
 console.table([accountId,accountEmail,accountPassword,accountCity]);
 
-console.log(accountEmail)
-console.log(accountId)
+console.log(accountEmail);
+console.log(accountId);
---
+++ basis_01/02_Datatype.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+
+console.log(3+3);
+let name ="anket"
+let age = 19
+let isLoggedIn = false
+// number=>2 to power 53
+//bigint 
+// String =>""
+// boolean = true/flse
+//Null = standalone value
+// undefined =>
+// symbol = unique
+
+
+//object
+ console.log( typeof name);
+ console.log(typeof null);
+ console.log(typeof undefined)
