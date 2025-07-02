bdf14301185c53b96683b21140d568661d2da52e
Aniket Naik <aniketnaik1985@gmail.com>
2025-07-02 16:28:35 +0530

    03

--- basis_01/03_Conversionoroperation.js
+++ basis_01/03_Conversionoroperation.js
@@ -11,3 +11,15 @@ console.log(valueInnumber);
 let islog = 1
 let booleanIslog = Boolean(islog)
 console.log(booleanIslog);
+ 
+let str1 ="hello"
+let str2 =" Friends"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
