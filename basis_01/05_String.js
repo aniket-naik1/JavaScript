170071aeafddf198c4f82bb7ee0f8b802f1a6a45
Aniket Naik <aniketnaik1985@gmail.com>
2025-07-06 14:29:50 +0530

    try

---
+++ basis_01/05_String.js
@@ -0,0 +1,29 @@
+ const name = "Aniket"
+ const repo = 2
+ console.log(name + repo + " value");
+ 
+ const Game = new String('Aniket')
+ console.log(Game[2]);
+ console.log(Game.length);
+ console.log(Game.toUpperCase());
+ console.log(Game.charAt(2));
+ console.log(Game.indexOf('t'));
+ console.log(Game);
+
+ const newS = Game.substring(0,4);
+ console.log(newS);
+
+ const anotherS = Game.slice(-6,4);
+ console.log(anotherS);
+
+ const one = "    DON    "
+ console.log(one);
+ console.log(one.trim());
+
+const url = "https://Aniket.com/An%100Naik"
+console.log(url.replace('%100','--'));
+ console.log(url.includes('An'));
+ 
+ 
+ 
+ 
