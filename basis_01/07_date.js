ad8cd08b71372f3709d8f54fe6bf788abf86bf52
Aniket Naik <aniketnaik1985@gmail.com>
2025-07-14 13:25:30 +0530

    07

---
+++ basis_01/07_date.js
@@ -0,0 +1,28 @@
+//Dates
+let myDate = new Date()
+console.log(myDate);
+console.log(myDate.toJSON());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+let createDate = new Date(2023 ,0,31)
+console.log(createDate.toDateString());
+
+
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(createDate.getTime());
+console.log(Date.now());
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMilliseconds());
+console.log(newDate.getDay());
+
+
+
+
+
+
+
